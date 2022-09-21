2ed542494003c86256582429d0bfd23aee5d8496
Leland Takamine <leland.takamine@gmail.com>
2022-09-21 15:21:57 -0700

    Implement Stream.Read

--- connection.go
+++ connection.go
@@ -25,9 +25,8 @@ type Stream struct {
 	localId    uint32
 	remoteId   uint32
 
-	ch             chan packet
-	packet         packet
-	packetPosition int
+	ch      chan packet
+	payload []byte
 }
 
 func Connect(conn net.Conn) (*Connection, error) {
@@ -100,14 +99,37 @@ func (c *Connection) getStreamChannel(localId uint32) chan packet {
 }
 
 func (s *Stream) Read(p []byte) (int, error) {
-	panic(0)
-}
-
-func (s *Stream) readPacket() (packet, error) {
-	ch := s.connection.getStreamChannel(s.localId)
-	if ch == nil {
-		return packet{}, fmt.Errorf("could not find channel for read: local id=0x%x", s.localId)
+	payload, err := s.getPayload()
+	if err != nil {
+		return 0, err
 	}
 
-	return <-ch, nil
+	n := copy(p, payload)
+
+	s.payload = payload[n:]
+
+	return n, nil
+}
+
+func (s *Stream) getPayload() ([]byte, error) {
+	if len(s.payload) > 0 {
+		return s.payload, nil
+	}
+
+	ch := s.connection.getStreamChannel(s.localId)
+	if ch == nil {
+		return nil, fmt.Errorf("could not find channel for read: local id=0x%x", s.localId)
+	}
+
+	p := <-ch
+
+	if p.Command == cmdClse {
+		return nil, io.EOF
+	}
+
+	if p.Command != cmdWrte {
+		return nil, fmt.Errorf("unexpected: command received 0x%x", p.Command)
+	}
+
+	return p.Payload, nil
 }
